7bf5355e4cd7e9994a3533e4f8b90967bb70d4e7
Alberto <tartana013@hotmail.com>
2026-06-30 08:41:09 +0200

    playbooks

---
+++ playbooks-laboratorios-2026/Chapter07-mariadb/insert.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS people (
+names varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+INSERT INTO people(names) VALUES('aLBERTO');
+INSERT INTO people(names) VALUES('Ana');
+INSERT INTO people(names) VALUES('Juan');
